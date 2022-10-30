9671ab9185922f6f045d6b6b5d04b46a2eb98e79
Javi Carrion <code-vista@outlook.com>
2022-10-29 21:59:07 -0400

    feat: :sparkles: hide native video controls

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.scss']
 })
-export class VideoPlayerComponent implements OnInit {
+export class VideoPlayerComponent implements OnInit, AfterViewInit {
+  @ViewChild('video')
+  video!: ElementRef;
+  @ViewChild('videoControls')
+  videoControls!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+   
   }
 
+  ngAfterViewInit(): void {
+    this.checkVideoWorks();
+  }
+
+  checkVideoWorks() {
+    const videoWorks = !!document.createElement('video').canPlayType;
+    if (videoWorks) {
+      this.video.nativeElement.controls = false;
+      this.videoControls.nativeElement.classList.remove('hidden');
+    }
+  }
 }
